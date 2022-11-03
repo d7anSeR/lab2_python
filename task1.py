27832b20d37116ab355fc9f5d9dfc0ac8e3d8ad6
daniya_kuspanova <kyspanova@icloud.com>
2022-11-03 20:55:44 +0400

    изменение абсолют и относ путей

--- task1.py
+++ task1.py
@@ -5,17 +5,20 @@ import os
 def writing_csv(name_class_good, name_class_bad, name_annatation) -> None:
     with open(name_annatation, mode="w", encoding="UTF-16", newline='') as f:
         writer = csv.writer(f, delimiter=';')
-        name_good_otnos = os.path.join("dataset", name_class_good)
-        name_bad_otnos = os.path.join("dataset", name_class_bad)
-        list_good = os.listdir(name_class_good)
-        list_bad = os.listdir(name_class_bad)
+        name_good = os.path.join("dataset", name_class_good)
+        name_bad = os.path.join("dataset", name_class_bad)
+        list_good = os.listdir(name_good)
+        list_bad = os.listdir(name_bad)
         for elem in list_good:
-            abspath_good = os.path.abspath(name_class_good)
+            abspath_good = os.path.join(os.path.abspath(name_class_good), elem)
+            name_good_otnos = os.path.join(name_good, elem)
             writer.writerow([abspath_good, name_good_otnos, name_class_good])
         for elem in list_bad:
-            abspath_bad = os.path.abspath(name_class_bad)
+            abspath_bad = os.path.join(os.path.abspath(name_class_bad), elem)
+            name_bad_otnos = os.path.join(name_bad, elem)
             writer.writerow([abspath_bad, name_bad_otnos, name_class_bad])
     pass
+    print("1 task completed")
 
 
 def run1(name_class_good, name_class_bad, name_annatation) -> None:
