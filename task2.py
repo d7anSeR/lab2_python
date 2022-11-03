8a07b80290d14f2d01ced7d7fecad9991e5f0713
daniya_kuspanova <kyspanova@icloud.com>
2022-11-03 20:58:25 +0400

    +в dir_create создание папок, измен путей, смещение цикла при csv

--- task2.py
+++ task2.py
@@ -4,16 +4,20 @@ import shutil
 
 
 def dir_create(dir_name) -> None:
+    name_dir_folder = os.path.join(dir_name, "dataset")
     if not os.path.isdir(dir_name):
         os.mkdir(dir_name)
+    if not os.path.isdir(name_dir_folder):
+        name = os.path.join(dir_name, "dataset")
+        os.mkdir(name)
 
 
 def copy_dir(dir_name, good_name, bad_name, annotation_name) -> None:
     dir_create(dir_name)
     good = os.path.join("dataset", good_name)
     bad = os.path.join("dataset", bad_name)
-    list_good = os.listdir(good_name)
-    list_bad = os.listdir(bad_name)
+    list_good = os.listdir(good)
+    list_bad = os.listdir(bad)
     for file_dataset in os.listdir("dataset"):
         path_p = os.path.join("dataset", file_dataset)
         class_name = os.listdir(path_p)
@@ -22,11 +26,11 @@ def copy_dir(dir_name, good_name, bad_name, annotation_name) -> None:
             new_file = os.path.join(
                 dir_name, "dataset", f"{file_dataset}_{elem}")
             shutil.copy(old_file, new_file)
-        with open(annotation_name, mode="w", encoding="UTF-16", newline='') as f:
-            writer = csv.writer(f, delimiter=';')
-            for elem in os.path.join(dir_name, "dataset"):
+            with open(annotation_name, mode="a", encoding="UTF-16", newline='') as f:
+                writer = csv.writer(f, delimiter=';')
                 writer.writerow([file_dataset, f"{file_dataset}_{elem}"])
-        pass
+            pass
+    print("2 task completed")
 
 
 def run2(dir_name, good_name, bad_name, annotation_name) -> None:
