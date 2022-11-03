7a195d05132799a49fa9eabc0fa267429de9808b
daniya_kuspanova <kyspanova@icloud.com>
2022-11-03 21:41:00 +0400

    добавлен run 3 задачи

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from task1 import run1
 from task2 import run2
+from task3 import run3
 
 
 if __name__ == '__main__':
     run1("good", "bad", "annotation1.csv")
-    run2("new_dir", "good", "bad", "annotation2.csv")
+    run2("new_dir_task2", "good", "bad", "annotation2.csv")
+    run3("new_dir_task3", "good", "bad", "annotation3.csv")
+# во всех файлах прописать docstring-и
